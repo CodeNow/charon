ea1b8ba8fd106e1ec4017d93e22aceec88576f1f
Ryan Sandor Richards <ryan@runnable.com>
2015-03-30 11:07:13 -0700

    Added simple tests.

---
+++ test/server.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var Lab = require('lab');
+var lab = exports.lab = Lab.script();
+var describe = lab.describe;
+var it = lab.it;
+var before = lab.before;
+var after = lab.after;
+var afterEach = lab.afterEach;
+var Code = require('code');
+var expect = Code.expect;
+var dns = require('native-dns');
+
+require('../lib/loadenv.js')();
+var server = require('../lib/server');
+
+describe('server', function() {
+  before(server.start);
+  after(server.stop);
+
+  function request(domain, cb) {
+    var req = dns.Request({
+      question: dns.Question({
+        name: domain,
+        type: 'A'
+      }),
+      server: {
+        address: '127.0.0.1',
+        port: process.env.PORT,
+        type: 'udp'
+      },
+      timeout: 1000
+    });
+    req.on('timeout', function () {
+      cb(new Error('DNS Server Timeout'));
+    });
+    req.on('message', cb);
+    req.send();
+  }
+
+  it('should resolve internal domain names', function (done) {
+    request('example.runnableapp.com', function (err, resp) {
+      expect(resp.answer).to.not.be.empty();
+      done();
+    });
+  });
+
+  it('should ignore external domain names', function (done) {
+    request('www.google.com', function (err, resp) {
+      expect(resp.answer).to.be.empty();
+      done();
+    });
+  });
+});
