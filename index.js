75f0323a9cbbe58afbc7fd31a13305301b520615
Ryan Sandor Richards <ryan@runnable.com>
2015-03-31 14:42:55 -0700

    Connect to API before starting DNS Server.
    * `index.js` now checks for and logs server start errors
    * Server's `.start` method now logs in to the api  before setting the dns server to listen
    * Server's '.stop` method now logs out of api before stopping the dns server
    * Server tests now ensure api login / logout occur
    * Minor tweaks to the server's tests for readability

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 'use strict';
 
+/**
+ * Starts the charon container-to-container DNS server.
+ * @module charon
+ * @author Ryan Sandor Richards
+ */
+
+var debug = require('debug');
 var server = require('./lib/server.js');
-server.start();
+
+var error = debug('charon:error');
+
+/**
+ * Callback to execute after server start.
+ * @param {Error} [err] Server start error, if applicable.
+ */
+function afterStart(err) {
+  error('Could not start server: ' + err);
+}
+
+server.start(afterStart);
--- test/server.js
+++ test/server.js
@@ -5,6 +5,7 @@ var lab = exports.lab = Lab.script();
 var describe = lab.describe;
 var it = lab.it;
 var before = lab.before;
+var beforeEach = lab.beforeEach;
 var after = lab.after;
 var afterEach = lab.afterEach;
 var Code = require('code');
@@ -18,8 +19,9 @@ require('../lib/loadenv.js')();
 var server = require('../lib/server');
 var rcode = require('../lib/dns-util').rcode;
 var query = require('../lib/query');
+var apiClient = require('../lib/api-client');
 
-function request(domain, cb) {
+function dnsRequest(domain, cb) {
   var req = dns.Request({
     question: dns.Question({ name: domain, type: 'A' }),
     server: {
@@ -38,6 +40,11 @@ function request(domain, cb) {
 
 describe('server', function() {
   describe('interface', function() {
+    it('should expose the server instance', function (done) {
+      expect(server.instance).to.exist();
+      done();
+    });
+
     it('should expose a `start` method', function (done) {
       expect(server.start).to.exist();
       expect(typeof server.start).to.equal('function');
@@ -51,21 +58,87 @@ describe('server', function() {
     });
   }); // end 'interface'
 
-  describe('DNS Requests', function() {
-    var serverInstance;
-
-    before(function (done) {
-      serverInstance = server.start();
+  describe('api integration', function() {
+    beforeEach(function (done) {
+      sinon.stub(server.instance, 'serve', function() {});
+      sinon.stub(server.instance, 'close', function() {});
       done();
+    });
+
+    afterEach(function (done) {
+      server.instance.serve.restore();
+      server.instance.close.restore();
+      done();
+    });
+
+    it('should login to the api on start', function (done) {
+      sinon.stub(apiClient, 'login', function (cb) {
+        cb();
+      });
+      server.start(function (err) {
+        if (err) { return done(err); }
+        expect(apiClient.login.calledOnce).to.be.true;
+        apiClient.login.restore();
+        done();
+      });
+    });
+
+    it('should logout from the api on stop', function (done) {
+      sinon.stub(apiClient, 'logout', function(cb) {
+        cb();
+      });
+      server.stop(function (err) {
+        if (err) { return done(err); }
+        expect(apiClient.logout.calledOnce).to.be.true;
+        apiClient.logout.restore();
+        done();
+      });
+    });
+
+    it('should not start the server if unable to login to the api', function (done) {
+      sinon.stub(apiClient, 'login', function (cb) {
+        cb(new Error('Login Error'));
+      });
+      server.start(function (err) {
+        expect(err).to.exist();
+        expect(server.instance.serve.callCount).to.equal(0);
+        apiClient.login.restore();
+        done();
+      });
+    });
+
+    it('should not stop the server if unable to logout of the api', function (done) {
+      sinon.stub(apiClient, 'logout', function (cb) {
+        cb(new Error('Logout Error'));
+      });
+      server.stop(function (err) {
+        expect(err).to.exist();
+        expect(server.instance.close.callCount).to.equal(0);
+        apiClient.logout.restore();
+        done();
+      });
+    });
+  }); // end 'api integration'
+
+  describe('DNS Requests', function() {
+    before(function (done) {
+      sinon.stub(apiClient, 'login', function(cb) { cb(); });
+      server.start(function() {
+        apiClient.login.restore();
+        done();
+      });
     });
 
     after(function (done) {
-      server.stop();
-      done();
+      sinon.stub(apiClient, 'logout', function(cb) { cb(); })
+      server.stop(function() {
+        apiClient.logout.restore();
+        done();
+      });
     });
 
     it('should resolve internal domain name requests', function (done) {
-      request('example.runnableapp.com', function (err, resp) {
+      dnsRequest('example.runnableapp.com', function (err, resp) {
         if (err) { return done(err); }
         expect(resp.answer).to.not.be.empty();
         expect(resp.header.rcode).to.equal(rcode.NoError);
@@ -74,7 +147,7 @@ describe('server', function() {
     });
 
     it('should deny external domain name requests', function (done) {
-      request('www.google.com', function (err, resp) {
+      dnsRequest('www.google.com', function (err, resp) {
         if (err) { return done(err); }
         expect(resp.answer).to.be.empty();
         expect(resp.header.rcode).to.equal(rcode.Refused);
@@ -86,7 +159,7 @@ describe('server', function() {
       sinon.stub(query, 'resolve', function (addr, domain, cb) {
         cb(new Error('Server error'), null);
       });
-      request('example.runnableapp.com', function (err, resp) {
+      dnsRequest('example.runnableapp.com', function (err, resp) {
         if (err) { return done(err); }
         expect(resp.answer).to.be.empty();
         expect(resp.header.rcode).to.equal(rcode.ServerFailure);
@@ -98,9 +171,9 @@ describe('server', function() {
     describe('error handling', function() {
       it('should report server errors', function (done) {
         sinon.stub(query, 'resolve', function () {
-          serverInstance.emit('error', new Error('ERROR'));
+          server.instance.emit('error', new Error('ERROR'));
         });
-        request('example.runnableapp.com', function (err, resp) {
+        dnsRequest('example.runnableapp.com', function (err, resp) {
           query.resolve.restore();
           done();
         });
@@ -108,9 +181,9 @@ describe('server', function() {
 
       it ('should report socket errors', function(done) {
         sinon.stub(query, 'resolve', function () {
-          serverInstance.emit('socketError', new Error('ERROR'));
+          server.instance.emit('socketError', new Error('ERROR'));
         });
-        request('example.runnableapp.com', function (err, resp) {
+        dnsRequest('example.runnableapp.com', function (err, resp) {
           query.resolve.restore();
           done();
         });
